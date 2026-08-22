ec04d9bf97989846b40bc3eb3d8ef50ef86c0c0b
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 00:57:52 +0530

    Create SecureDroidPlugin.kt

---
+++ android/app/src/main/java/org/securedroid/app/SecureDroidPlugin.kt
@@ -0,0 +1,32 @@
+package org.securedroid.app
+
+import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.annotation.CapacitorPlugin
+
+@CapacitorPlugin(name = "SecureDroid")
+class SecureDroidPlugin : Plugin() {
+
+    private lateinit var deviceInfoManager: DeviceInfoManager
+    private lateinit var batteryManagerHelper: BatteryManagerHelper
+
+    override fun load() {
+        deviceInfoManager = DeviceInfoManager(context)
+        batteryManagerHelper = BatteryManagerHelper(context)
+    }
+
+    @PluginMethod
+    fun getDeviceInfo(call: PluginCall) {
+        try {
+            val res = deviceInfoManager.getRealDeviceInfo()
+            call.resolve(res)
+        } catch (e: Exception) {
+            val err = JSObject()
+            err.put("success", false)
+            err.put("message", e.localizedMessage)
+            call.resolve(err)
+        }
+    }
+}
