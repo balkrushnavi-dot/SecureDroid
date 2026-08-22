af9917937b021791c5031dc41988c692b6aac427
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 04:45:34 +0530

    Update MainActivity.kt

--- android/app/src/main/java/org/securedroid/app/MainActivity.kt
+++ android/app/src/main/java/org/securedroid/app/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
 class MainActivity : BridgeActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        registerPlugin(SecureDroidPlugin::class.java)
         super.onCreate(savedInstanceState)
     }
 }
