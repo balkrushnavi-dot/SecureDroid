71998f3ce9c9783c718520a7745651ee60354904
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 01:00:22 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/java/org/securedroid/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.securedroid.app
+
+import android.os.Bundle
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        registerPlugin(SecureDroidPlugin::class.java)
+        super.onCreate(savedInstanceState)
+    }
+}
