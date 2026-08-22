b98f1160215b1a54b0af55232322d57caaa590bc
balkrushnavi-dot <balkrushnavi@gmail.com>
2026-08-23 01:45:35 +0530

    Update SecureDroidPlugin.ts

--- src/services/native/SecureDroidPlugin.ts
+++ src/services/native/SecureDroidPlugin.ts
@@ -1,9 +1,11 @@
 import { registerPlugin } from '@capacitor/core';
-import { NativeResult, RealDeviceInfo, RealBatteryStatus } from '../../types/native';
+import { NativeResult, RealDeviceInfo, RealBatteryStatus, RealNetworkState, RealStorageState } from '../../types/native';
 
 export interface SecureDroidPlugin {
   getDeviceInfo(): Promise<NativeResult<RealDeviceInfo>>;
   getBatteryStatus(): Promise<NativeResult<RealBatteryStatus>>;
+  getNetworkState(): Promise<NativeResult<RealNetworkState>>;
+  getStorageState(): Promise<NativeResult<RealStorageState>>;
 }
 
 export const SecureDroidNative = registerPlugin<SecureDroidPlugin>('SecureDroid');
